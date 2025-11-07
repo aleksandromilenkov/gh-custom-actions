16066b0b006281a61a4f3c8bf275deb54499bc32
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-11-07 18:50:35 +0100

    retreiving inputs in the js file and executing call to aws to upload the dist folder in the specific bucket in the specific region

--- .github/actions/deploying-s3-javascript/main.js
+++ .github/actions/deploying-s3-javascript/main.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const github = require('@actions/github');// can communicate with github API
 const exec = require('@actions/exec');
 
 function run() {
     // 1) Get some input values
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const region = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files to S3
+    // will be executed in a regular shell in our runner
+    const bucketUri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${bucketUri} --region ${region}`);
     core.notice('Hello from my custom JavaScript action!');
 }
 
